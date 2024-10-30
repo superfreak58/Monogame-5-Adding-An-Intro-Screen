2bde66cdcc4fd7dc9ba6901afab1cbbe8dcefbd2
superfreak58 <rayjkolyn@gmail.com>
2024-10-30 09:39:46 -0400

    ngfnf

--- Game1.cs
+++ Game1.cs
@@ -70,71 +70,45 @@ namespace Monogame_5__Adding_An_Intro_Screen
             {
                 if (mouseState.LeftButton == ButtonState.Pressed)
                     screen = Screen.TribbleYard;
-                greyTribbleRec.X += (int)greyTribbleSpeed.X;
-
-                greyTribbleRec.Y += (int)greyTribbleSpeed.Y;
-
-                if (greyTribbleRec.Right >= window.Width || greyTribbleRec.X <= 0)
-                    greyTribbleSpeed.X *= -1;
-                if (greyTribbleRec.Bottom >= window.Height || greyTribbleRec.Top <= 0)
-                    greyTribbleSpeed.Y *= -1;
-
-                brownTribbleRec.X += (int)brownTribbleSpeed.X;
-
-                brownTribbleRec.Y += (int)brownTribbleSpeed.Y;
-
-                if (brownTribbleRec.Right >= window.Width || brownTribbleRec.X <= 0)
-                    brownTribbleRec.X = 0;
-                if (brownTribbleRec.Bottom >= window.Height || brownTribbleRec.Top <= 0)
-                    brownTribbleSpeed.Y *= -1;
-
-                creamTribbleRec.X += (int)creamTribbleSpeed.X;
-                creamTribbleRec.Y += (int)creamTribbleSpeed.Y;
-
-                if (creamTribbleRec.Right >= window.Width || creamTribbleRec.X <= 0)
+               
                 {
-                    creamTribbleSpeed.X *= -1;
-
 
                 }
-                if (creamTribbleRec.Bottom >= window.Height || creamTribbleRec.Top <= 0)
-                    creamTribbleSpeed.Y *= -1;
 
+                base.Update(gameTime);
             }
-            else if (screen == Screen.TribbleYard)
-            {
-                
-            }
-
-            base.Update(gameTime);
         }
-
         protected override void Draw(GameTime gameTime)
         {
+            _spriteBatch.Begin();
+
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
-            _spriteBatch.Begin();
+            
             if (screen == Screen.Intro)
             {
-                _spriteBatch.Draw(tribbleIntroTexture, new Rectangle(0, 0, 800, 500), Color.White);
+
+                _spriteBatch.Draw(tribbleIntroTexture, new Rectangle(0, 0, 800, 600), Color.White);
+
+
             }
             else if (screen == Screen.TribbleYard)
             {
                 // Your previous tribble drawing code should go here
+
                 
-                _spriteBatch.Begin();
                 _spriteBatch.Draw(greyTribbleTexture, greyTribbleRec, Color.White);
                 _spriteBatch.Draw(brownTribbleTexture, brownTribbleRec, Color.White);
                 _spriteBatch.Draw(creamTribbleTexture, creamTribbleRec, Color.White);
-                _spriteBatch.End();
-
 
 
             }
             
+            _spriteBatch.End();
 
             base.Draw(gameTime);
+
         }
     }
 }
