5cfdcf3837759c8609607c242cf66297671d035e
superfreak58 <rayjkolyn@gmail.com>
2024-10-31 09:31:29 -0400

    we are so back

--- Game1.cs
+++ Game1.cs
@@ -43,7 +43,7 @@ namespace Monogame_5__Adding_An_Intro_Screen
             _graphics.PreferredBackBufferHeight = window.Height;
             _graphics.ApplyChanges();
 
-            
+
 
             base.Initialize();
         }
@@ -70,14 +70,46 @@ namespace Monogame_5__Adding_An_Intro_Screen
             {
                 if (mouseState.LeftButton == ButtonState.Pressed)
                     screen = Screen.TribbleYard;
-               
+            }
+            if (screen == Screen.TribbleYard)
+            {
+                greyTribbleRec.X += (int)greyTribbleSpeed.X;
+
+                greyTribbleRec.Y += (int)greyTribbleSpeed.Y;
+
+                if (greyTribbleRec.Right >= window.Width || greyTribbleRec.X <= 0)
+                    greyTribbleSpeed.X *= -1;
+                if (greyTribbleRec.Bottom >= window.Height || greyTribbleRec.Top <= 0)
+                    greyTribbleSpeed.Y *= -1;
+
+                brownTribbleRec.X += (int)brownTribbleSpeed.X;
+
+                brownTribbleRec.Y += (int)brownTribbleSpeed.Y;
+
+                if (brownTribbleRec.Right >= window.Width || brownTribbleRec.X <= 0)
+                    brownTribbleRec.X = 0;
+                if (brownTribbleRec.Bottom >= window.Height || brownTribbleRec.Top <= 0)
+                    brownTribbleSpeed.Y *= -1;
+
+                creamTribbleRec.X += (int)creamTribbleSpeed.X;
+                creamTribbleRec.Y += (int)creamTribbleSpeed.Y;
+
+                if (creamTribbleRec.Right >= window.Width || creamTribbleRec.X <= 0)
                 {
+                    creamTribbleSpeed.X *= -1;
+
 
                 }
+                if (creamTribbleRec.Bottom >= window.Height || creamTribbleRec.Top <= 0)
+                    creamTribbleSpeed.Y *= -1;
+
+
+
 
                 base.Update(gameTime);
             }
         }
+
         protected override void Draw(GameTime gameTime)
         {
             _spriteBatch.Begin();
@@ -85,7 +117,7 @@ namespace Monogame_5__Adding_An_Intro_Screen
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
-            
+
             if (screen == Screen.Intro)
             {
 
@@ -97,14 +129,14 @@ namespace Monogame_5__Adding_An_Intro_Screen
             {
                 // Your previous tribble drawing code should go here
 
-                
+
                 _spriteBatch.Draw(greyTribbleTexture, greyTribbleRec, Color.White);
                 _spriteBatch.Draw(brownTribbleTexture, brownTribbleRec, Color.White);
                 _spriteBatch.Draw(creamTribbleTexture, creamTribbleRec, Color.White);
 
 
             }
-            
+
             _spriteBatch.End();
 
             base.Draw(gameTime);
@@ -112,3 +144,4 @@ namespace Monogame_5__Adding_An_Intro_Screen
         }
     }
 }
+
